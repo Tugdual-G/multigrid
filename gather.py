eb2da5af29d615d3055d3c900df92595154db34f
Tugdual-G <tugducarfantan@gmail.com>
2021-11-25 23:57:29 +0100

    add offset numba module, split not working when compiled

--- gather.py
+++ gather.py
@@ -4,55 +4,56 @@ from numba import jit
 import matplotlib.pyplot as plt
 from mpi4py import MPI
 from parallel_multigrid import Buffers
+from multigrid_module import interpolate_add_to, coarse
 
-def interpolate_add_to(a, a_new, ofst_i=0, ofst_j=0):
-    """Interpolate."""
-    for j in range(1, a.shape[0] - 1):
-        for i in range(1, a.shape[1] - 1):
-            a_new[2 * j+ofst_j, 2 * i+ofst_i] += a[j, i]
-    for j in range(0, a.shape[0] - 1):
-        for i in range(0, a.shape[1] - 1):
-            a_new[2 * j + 1 + ofst_j, 2 * i + 1 + ofst_i] += (
-                a[j + 1, i + 1] + a[j + 1, i] + a[j, i + 1] + a[j, i]
-            ) / 4
-    for j in range(1, a.shape[0] - 1):
-        for i in range(0, a.shape[1] - 1):
-            a_new[2 * j + ofst_j, 2 * i + 1 + ofst_i] += (a[j, i] + a[j, i + 1]) / 2
+# def interpolate_add_to(a, a_new, ofst_i=0, ofst_j=0):
+#     """Interpolate."""
+#     for j in range(1, a.shape[0] - 1):
+#         for i in range(1, a.shape[1] - 1):
+#             a_new[2 * j+ofst_j, 2 * i+ofst_i] += a[j, i]
+#     for j in range(0, a.shape[0] - 1):
+#         for i in range(0, a.shape[1] - 1):
+#             a_new[2 * j + 1 + ofst_j, 2 * i + 1 + ofst_i] += (
+#                 a[j + 1, i + 1] + a[j + 1, i] + a[j, i + 1] + a[j, i]
+#             ) / 4
+#     for j in range(1, a.shape[0] - 1):
+#         for i in range(0, a.shape[1] - 1):
+#             a_new[2 * j + ofst_j, 2 * i + 1 + ofst_i] += (a[j, i] + a[j, i + 1]) / 2
 
-    for j in range(0, a.shape[0] - 1):
-        for i in range(1, a.shape[1] - 1):
-            a_new[2 * j + 1 + ofst_j, 2 * i + ofst_i] += (a[j, i] + a[j + 1, i]) / 2
+#     for j in range(0, a.shape[0] - 1):
+#         for i in range(1, a.shape[1] - 1):
+#             a_new[2 * j + 1 + ofst_j, 2 * i + ofst_i] += (a[j, i] + a[j + 1, i]) / 2
+
+
+# @jit(nopython=True, cache=True)
+# def coarse(a, a_crs, ofst_i=0, ofst_j=0):
+#     """Reduction on coarser grid."""
+#     for j in range(1, a_crs.shape[0] - 1):
+#         a_left = a[2 * j+ofst_j, 1+ofst_i] / 8 + (a[2 * j+ofst_j + 1, 1+ofst_i] + a[2 * j+ofst_j - 1, 1+ofst_i]) / 16
+#         for i in range(1, a_crs.shape[1] - 1):
+#             a_right = (
+#                 a[2 * j+ofst_j, 2 * i+ofst_i + 1] / 8
+#                 + (a[2 * j+ofst_j + 1, 2 * i+ofst_i + 1] + a[2 * j+ofst_j - 1, 2 * i+ofst_i + 1]) / 16
+#             )
+#             a_crs[j, i] = (
+#                 a[2 * j+ofst_j, 2 * i+ofst_i] / 4
+#                 + (a[2 * j+ofst_j + 1, 2 * i+ofst_i] + a[2 * j+ofst_j - 1, 2 * i+ofst_i]) * 1 / 8
+#             )
+#             a_crs[j, i] += a_right + a_left
+#             a_left = a_right
 
 
 @jit(nopython=True, cache=True)
-def coarse(a, a_crs, ofst_i=0, ofst_j=0):
-    """Reduction on coarser grid."""
-    for j in range(1, a_crs.shape[0] - 1):
-        a_left = a[2 * j+ofst_j, 1+ofst_i] / 8 + (a[2 * j+ofst_j + 1, 1+ofst_i] + a[2 * j+ofst_j - 1, 1+ofst_i]) / 16
-        for i in range(1, a_crs.shape[1] - 1):
-            a_right = (
-                a[2 * j+ofst_j, 2 * i+ofst_i + 1] / 8
-                + (a[2 * j+ofst_j + 1, 2 * i+ofst_i + 1] + a[2 * j+ofst_j - 1, 2 * i+ofst_i + 1]) / 16
-            )
-            a_crs[j, i] = (
-                a[2 * j+ofst_j, 2 * i+ofst_i] / 4
-                + (a[2 * j+ofst_j + 1, 2 * i+ofst_i] + a[2 * j+ofst_j - 1, 2 * i+ofst_i]) * 1 / 8
-            )
-            a_crs[j, i] += a_right + a_left
-            a_left = a_right
-
-
-@jit(nopython=True, cache=True)
-def split(rank, A_in, A_out):
+def split(A_in, A_out, rank):
     nx_out, _ = A_out.shape
     if rank == 0:
-        A_out[:] = A[0: nx_out, 0: nx_out]
+        A_out[:] = A_in[0: nx_out, 0: nx_out]
     elif rank == 1:
-        A_out[:] = A[0: nx_out, -nx_out:]
+        A_out[:] = A_in[0: nx_out, -nx_out:]
     elif rank == 2:
-        A_out[:] = A[-nx_out:, 0: nx_out]
+        A_out[:] = A_in[-nx_out:, 0: nx_out]
     elif rank == 3:
-        A_out[:] = A[-nx_out:, -nx_out:]
+        A_out[:] = A_in[-nx_out:, -nx_out:]
 
 
 
@@ -108,7 +109,7 @@ if __name__ == "__main__":
     buf = [Buffers(sub[i].shape, sub[i], 1, 1) for i in range(depth_para+1)]
     # sub[-1] is a temporary buffer to store data before gathering or spliting
 
-    split(rank, A, sub[0])
+    split(A, sub[0], rank)
     buf[0].fill_buffers()
     buf[0].fill_var()
     buf[0].fill_buffers()
@@ -132,17 +133,17 @@ if __name__ == "__main__":
     #
     gather_blocks(comm, sub[-1], whole[0])
     for i in range(n-depth_para):
-        coarse(whole[i], whole[i+1])
+        coarse(whole[i], whole[i+1], 0, 0)
 
     ###############################
     #           Ascent            #
     ###############################
     for i in range(1, n-depth_para+1):
         whole[-i-1][:] = 0
-        interpolate_add_to(whole[-i], whole[-i-1])
+        interpolate_add_to(whole[-i], whole[-i-1], 0, 0)
 
     # _________Sub domain
-    split(rank, whole[0], sub[-1])
+    split(whole[0], sub[-1], rank)
     for i in range(1, depth_para):
         interpolate_add_to(sub[-i], sub[-i-1], ofst["i"], ofst["j"])
         if rank == 3:
--- numba_modules.py
+++ numba_modules.py
@@ -6,9 +6,9 @@ cc = CC('multigrid_module')
 # Uncomment the following line to print out the compilation steps
 #cc.verbose = True
 
-@cc.export('split', '(u1, f8[:, :], f8[:, :])')
-def split(rank, A_in, A_out):
-    nx_out, _ = A_out.shape
+@cc.export('split', '(f8[:, :], f8[:, :], u1)')
+def split(A_in, A_out, rank):
+    nx_out, ny = A_out.shape
     if rank == 0:
         A_out[:] = A_in[0: nx_out, 0: nx_out]
     elif rank == 1:
@@ -68,42 +68,42 @@ def smooth(b, x, h, lplc, r, iterations):
             ) / (h ** 2)
     r[:] = b - lplc
 
-@cc.export('coarse', '(f8[:, :], f8[:, :])')
-def coarse(a, a_crs):
+@cc.export('coarse', '(f8[:, :], f8[:, :], i1, i1)')
+def coarse(a, a_crs, ofst_i=0, ofst_j=0):
     """Reduction on coarser grid."""
     for j in range(1, a_crs.shape[0] - 1):
-        a_left = a[2 * j, 1] / 8 + (a[2 * j + 1, 1] + a[2 * j - 1, 1]) / 16
+        a_left = a[2 * j+ofst_j, 1+ofst_i] / 8 + (a[2 * j+ofst_j + 1, 1+ofst_i] + a[2 * j+ofst_j - 1, 1+ofst_i]) / 16
         for i in range(1, a_crs.shape[1] - 1):
             a_right = (
-                a[2 * j, 2 * i + 1] / 8
-                + (a[2 * j + 1, 2 * i + 1] + a[2 * j - 1, 2 * i + 1]) / 16
+                a[2 * j+ofst_j, 2 * i+ofst_i + 1] / 8
+                + (a[2 * j+ofst_j + 1, 2 * i+ofst_i + 1] + a[2 * j+ofst_j - 1, 2 * i+ofst_i + 1]) / 16
             )
             a_crs[j, i] = (
-                a[2 * j, 2 * i] / 4
-                + (a[2 * j + 1, 2 * i] + a[2 * j - 1, 2 * i]) * 1 / 8
+                a[2 * j+ofst_j, 2 * i+ofst_i] / 4
+                + (a[2 * j+ofst_j + 1, 2 * i+ofst_i] + a[2 * j+ofst_j - 1, 2 * i+ofst_i]) * 1 / 8
             )
             a_crs[j, i] += a_right + a_left
             a_left = a_right
 
 
-@cc.export('interpolate_add_to', '(f8[:, :], f8[:, :])')
-def interpolate_add_to(a, a_new):
+@cc.export('interpolate_add_to', '(f8[:, :], f8[:, :], i1, i1)')
+def interpolate_add_to(a, a_new, ofst_i=0, ofst_j=0):
     """Interpolate."""
     for j in range(1, a.shape[0] - 1):
         for i in range(1, a.shape[1] - 1):
-            a_new[2 * j, 2 * i] += a[j, i]
+            a_new[2 * j+ofst_j, 2 * i+ofst_i] += a[j, i]
     for j in range(0, a.shape[0] - 1):
         for i in range(0, a.shape[1] - 1):
-            a_new[2 * j + 1, 2 * i + 1] += (
+            a_new[2 * j + 1 + ofst_j, 2 * i + 1 + ofst_i] += (
                 a[j + 1, i + 1] + a[j + 1, i] + a[j, i + 1] + a[j, i]
             ) / 4
     for j in range(1, a.shape[0] - 1):
         for i in range(0, a.shape[1] - 1):
-            a_new[2 * j, 2 * i + 1] += (a[j, i] + a[j, i + 1]) / 2
+            a_new[2 * j + ofst_j, 2 * i + 1 + ofst_i] += (a[j, i] + a[j, i + 1]) / 2
 
     for j in range(0, a.shape[0] - 1):
         for i in range(1, a.shape[1] - 1):
-            a_new[2 * j + 1, 2 * i] += (a[j, i] + a[j + 1, i]) / 2
+            a_new[2 * j + 1 + ofst_j, 2 * i + ofst_i] += (a[j, i] + a[j + 1, i]) / 2
 
 if __name__ == "__main__":
     cc.compile()
